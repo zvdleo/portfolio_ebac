8455e3310136765508d56531e045fb5ca7f8bf95
Leonardo Santos Azevedo <leonardosantosazevedo195@gmail.com>
2024-04-03 18:19:07 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/zvdleo.png" alt="" />
-    <Titulo fontSize={20}>Leonardo Azevedo</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Leonardo Azevedo</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        zvdleo
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-End
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
