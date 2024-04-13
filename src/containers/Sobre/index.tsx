d57151038f2107525a6394bb0ebb69ab69700224
Leonardo Santos Azevedo <leonardosantosazevedo195@gmail.com>
2024-04-13 00:54:09 -0300

    ajuste do sobre mim

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -4,12 +4,15 @@ import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
-    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Titulo fontSize={16}>About me</Titulo>
     <Paragrafo tipo="principal">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-      pariatur, debitis fuga laudantium natus sequi dignissimos placeat
-      suscipit? Tenetur nihil, officia aliquam inventore nostrum incidunt cumque
-      in dolorem eligendi. Laboriosam!
+      Hello! I am a programming and systems development enthusiast, with a
+      special passion for the Full Stack area. Since then, I have dedicated my
+      time and energy to constantly improving this knowledge. I am a firm
+      believer in the power of continuous learning and have been actively
+      seeking opportunities to expand my expertise. My determination to master
+      my craft is reflected in my pursuit of challenges that encourage me to
+      grow as a professional.
     </Paragrafo>
     <GithubSecao>
       <img src="https://github-readme-stats.vercel.app/api?username=zvdleo&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
